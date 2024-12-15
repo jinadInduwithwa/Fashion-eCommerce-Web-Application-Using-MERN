f1a185ffac5ff01481b4212d7acd3817b6b1fbf6
JINAD INDUWITHWA A.G <140488745+jinadInduwithwa@users.noreply.github.com>
2024-12-15 21:40:57 +0530

    product router

---
+++ backend/controllers/productController.js
@@ -0,0 +1,23 @@
+
+// route for add product
+const addProduct = async(req,res) =>{
+
+}
+
+// route for list product
+const listProduct = async(req,res) =>{
+    
+}
+
+
+// route for remove product
+const removeProduct = async(req,res) =>{
+    
+}
+
+// route for single product infomation
+const singleProduct = async(req,res) =>{
+    
+}
+
+export {addProduct, listProduct, removeProduct, singleProduct}
