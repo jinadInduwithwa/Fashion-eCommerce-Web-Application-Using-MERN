12ec302c9ccb9aa8ce6eb3780797a92c5917e0b0
JINAD INDUWITHWA A.G <140488745+jinadInduwithwa@users.noreply.github.com>
2025-01-01 07:09:54 +0530

    product add admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,25 +8,23 @@ import Orders from './pages/Orders';
 import Login from './components/Login';
 import { ToastContainer } from 'react-toastify';
 
-
-export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
-
-  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
+  const [token, setToken] = useState(localStorage.getItem('token') || '');
 
   useEffect(() => {
-    localStorage.setItem('token',token)
-  },[token])
+    localStorage.setItem('token', token);
+  }, [token]);
 
   return (
     <div className="bg-gray-50 min-h-screen">
-      <ToastContainer/>
-      {token === "" ? (
-        <Login setToken={setToken}/>
+      <ToastContainer />
+      {token === '' ? (
+        <Login setToken={setToken} />
       ) : (
         <>
-          <Navbar setToken={setToken}/>
+          <Navbar setToken={setToken} />
           <hr />
           <div className="flex w-full">
             <Sidebar />
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const Navbar = ({setToken}) => {
+const Navbar = ({ setToken }) => {
+  
+
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
-        <h1>Admin</h1>
-      <button onClick={()=>setToken('')} className="bg-gray-600 px-5 py-2 rounded-full sm:py-2 sm:px-7 text-white">Log out</button>
+      <h1>Admin</h1>
+      <button
+        onClick={setToken('')}
+        className="bg-gray-600 px-5 py-2 rounded-full sm:py-2 sm:px-7 text-white hover:bg-gray-500 transition-all"
+      >
+        Log out
+      </button>
     </div>
   );
 };
